b4d6169e189fd27949ed2db85fbf935ed8ac80a9
Aaryan Murgunde (Spectre Jammy) <aaryan.murgunde@gmail.com>
2023-12-06 16:43:57 +0530

    docker name update
    Signed-off-by: Aaryan Murgunde (Spectre Jammy) <aaryan.murgunde@gmail.com>

--- port_files.sh
+++ port_files.sh
@@ -109,8 +109,8 @@ create_dir() {
 	mkdir -p $WORKING_DIR/docs
 	echo -e "${GREEN}\t- docs${ENDCOLOR}"
 
-	mkdir -p $WORKING_DIR/Docker
-	echo -e "${GREEN}\t- Docker${ENDCOLOR}"
+	mkdir -p $WORKING_DIR/docker
+	echo -e "${GREEN}\t- docker${ENDCOLOR}"
 
 	echo -e "${GREEN}Created directories${ENDCOLOR}\n"
 }
--- start.sh
+++ start.sh
@@ -118,8 +118,8 @@ echo -e "${GREEN}\t- packages${ENDCOLOR}"
 mkdir -p $WORKING_DIR/docs
 echo -e "${GREEN}\t- docs${ENDCOLOR}"
 
-mkdir -p $WORKING_DIR/Docker
-echo -e "${GREEN}\t- Docker${ENDCOLOR}"
+mkdir -p $WORKING_DIR/docker
+echo -e "${GREEN}\t- docker${ENDCOLOR}"
 
 echo -e "${GREEN}Created directories${ENDCOLOR}\n"
 
